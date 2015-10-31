f48489dc45d2b8b761b9b3fe54f512f9cc198c46
Janne Lönn <janne.o.harju@gmail.com>
2015-10-31 11:50:58 +0100

    Init link script

---
+++ link.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+DOTFILES="bashrc inputrc tmux-conf vimrc vim zshrc"
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for DOTFILE in $DOTFILES; do
+  rm -f ~/.$DOTFILE
+  ln -s $DOTFILES_DIR/.$DOTFILE ~/.$DOTFILE
+done
