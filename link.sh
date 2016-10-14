44424796cab7b71b0382c4427cd00694ff26c9e1
Janne Lönn <janne@izettle.com>
2016-10-14 08:55:42 +0200

    Fix tmux conf

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-DOTFILES="bashrc bash_profile inputrc tmux-conf vimrc vim zshrc gitconfig"
+DOTFILES="bashrc bash_profile inputrc tmux.conf vimrc vim zshrc gitconfig"
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for DOTFILE in $DOTFILES; do
